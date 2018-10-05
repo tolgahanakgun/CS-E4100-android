ad279d6b0fe876dd4a3b4b4df39de041d8ccf0b9
Tolgahan AKGUN <akguntolgahan@gmail.com>
2018-10-06 00:39:22 +0300

    Refactor sort methods

--- ImageGrid/app/src/main/java/fi/imagegrid/MainActivity.java
+++ ImageGrid/app/src/main/java/fi/imagegrid/MainActivity.java
@@ -1,15 +1,20 @@
 package fi.imagegrid;
 
+import android.content.Context;
 import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.GridView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private final static int SORT_BY_ASCENDING = 1;
+    private final static int SORT_BY_DESCENDING = 2;
+    private final static int SORT_BY_RECENT = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,21 +32,13 @@ public class MainActivity extends AppCompatActivity {
         for(int index =0; index <jsonImages.getImages().size();index++){
             vi.setText(vi.getText()+"\t"+String.valueOf(index)+" - "+((JSONImage)jsonImages.getImages().get(index)).getAuthor()+"\n");
         }
-        //final int size = jsonImages.getImages().size();
-        //Button b = findViewById(R.id.button);
-        //b.setText(String.valueOf(size));
     }
 
     public void sortAscending(View view){
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                EditText editText = findViewById(R.id.txtUrl);
-                String url = editText.getText().toString();
-                JSONImages jsonImages = new JSONImages(url);
-                jsonImages.sortByAuthorAscending();
-                Button b = findViewById(R.id.sortAsc);
-                b.setText(jsonImages.getImages().get(0).getAuthor());
+                sortAndShow(SORT_BY_ASCENDING);
             }
         });
     }
@@ -50,12 +47,7 @@ public class MainActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                EditText editText = findViewById(R.id.txtUrl);
-                String url = editText.getText().toString();
-                JSONImages jsonImages = new JSONImages(url);
-                jsonImages.sortByAuthorDescending();
-                Button b = findViewById(R.id.sortDes);
-                b.setText(jsonImages.getImages().get(0).getAuthor());
+                sortAndShow(SORT_BY_DESCENDING);
             }
         });
     }
@@ -64,13 +56,35 @@ public class MainActivity extends AppCompatActivity {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                EditText editText = findViewById(R.id.txtUrl);
-                String url = editText.getText().toString();
-                JSONImages jsonImages = new JSONImages(url);
-                jsonImages.sortByDateRecentFirst();
-                Button b = findViewById(R.id.sortDate);
-                b.setText(jsonImages.getImages().get(0).getAuthor());
+                sortAndShow(SORT_BY_RECENT);
             }
         });
+    }
+
+    private void sortAndShow(int sortBy){
+        EditText editText = findViewById(R.id.txtUrl);
+        String url = editText.getText().toString();
+        JSONImages jsonImages = new JSONImages(url);
+        switch (sortBy){
+            case SORT_BY_ASCENDING:
+                jsonImages.sortByAuthorAscending();
+                break;
+            case SORT_BY_DESCENDING:
+                jsonImages.sortByAuthorDescending();
+                break;
+            case SORT_BY_RECENT:
+                jsonImages.sortByDateRecentFirst();
+                break;
+        }
+        String[] urls = new String[jsonImages.getImages().size()];
+        for(int index=0;index<jsonImages.getImages().size();index++){
+            urls[index] = jsonImages.getImages().get(index).getUrl().toString();
+        }
+        GridView gridView = findViewById(R.id.gridView);
+        gridView.setAdapter(new SampleGridViewAdapter(getContext(),urls));
+    }
+
+    private Context getContext(){
+        return this;
     }
 }
